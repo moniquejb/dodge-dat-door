bb5490245a8a2ff3fe80e20a954fef530523d3ce
Monique Blignaut <54107967+moniquejb@users.noreply.github.com>
2021-10-21 10:46:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,10 @@ let doorsArr = document.getElementById('door-group').children;
 
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
+window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
 
 let newPinkDoor, newBlueDoor, newPurpleDoor, newOrangeDoor;
 let hasClickedPink = false;

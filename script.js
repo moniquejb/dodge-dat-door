52ab3763d61c9da716497c2d4ca321d984483eba
Monique Blignaut <54107967+moniquejb@users.noreply.github.com>
2021-10-21 10:53:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -16,6 +16,16 @@ window.addEventListener('resize', () => {
     document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
 
+screen.orientation.addEventListener('change', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+})
+
+screen.orientation.onchange = () => { 
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
 let newPinkDoor, newBlueDoor, newPurpleDoor, newOrangeDoor;
 let hasClickedPink = false;
 let hasClickedBlue = false;
